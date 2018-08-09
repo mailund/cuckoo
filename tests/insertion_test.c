ebd4443750ec2d909f8c44dc64771a7bcdf35708
Thomas Mailund <mailund@birc.au.dk>
2018-08-09 15:34:53 +0200

    changed runtime warning to compile time warming

--- tests/insertion_test.c
+++ tests/insertion_test.c
@@ -7,8 +7,8 @@ int main(int argc, const char *argv[])
     const size_t initial_size = 8;
     struct cuckoo_set *set = new_cuckoo_set(initial_size, 0, 0);
     assertion_msg(set != 0, "Failed to create set.");
-    
-    warning("Test not implemented yet.");
+
+#warning Test not implemented yet.
     
     return EXIT_SUCCESS;
 }
